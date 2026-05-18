ecf2f0598f83be334960df223772d64977473fa5
Christian Johansen <christian@cjohansen.no>
2026-05-18 08:45:44 +0200

    Fix lint error

--- src/powerblog/export.clj
+++ src/powerblog/export.clj
@@ -2,7 +2,7 @@
   (:require [powerblog.core :as blog]
             [powerpack.export :as export]))
 
-(defn ^:export export! [& args]
+(defn ^:export export! [& _args]
   (-> blog/config
       (assoc :site/base-url "https://www.example.com")
       export/export!))
